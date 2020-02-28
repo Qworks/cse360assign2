2a10717880b90788b583eba20f2f386c842c466c
Ben Hay <benhay81@gmail.com>
2020-02-27 19:40:03 -0700

    Added append, first, last, and size functions

--- assign1/src/cse360assign2/SimpleList.java
+++ assign1/src/cse360assign2/SimpleList.java
@@ -1,12 +1,13 @@
 /**
  * @author Benjamin Hay
  * Class ID: 321
- * Assignment Number:1
+ * Assignment Number:2
  * Description: This Class makes a simple list in an array
  * where integers can be added and removed. This version 
  * increases the size of the array by 50% when adding if it
  * is full and decreases the size by 25% when removing if at least 25% of
- * the array is empty.
+ * the array is empty. It also includes four new functions: append, first,
+ * last, and size.
  */
 package cse360assign2;
 
@@ -123,5 +124,58 @@ public class SimpleList
 			}
 		}
 		return -1;
+	}
+	public void append(int num)
+	/**
+	 * appends the integer to the end of the list and increases
+	 * the size of the list by 50% if it is full.
+	 * @param num number to be appended
+	 */
+	{
+		if(count == list.length)//increases the size if the list is full
+		{
+			int[] temp = new int[(int) (list.length * 1.5)]; 
+			//increases the size by 50% rounded down
+			for(int index2 = 0; index2 < count; index2++) 
+			//copies list into a temp array
+			{
+				temp[index2] = list[index2]; 
+			}
+			list = temp; //list becomes temp
+		}
+		list[count] = num;
+		count++;
+	}
+	public int first()
+	/**
+	 * Returns the first element of the list
+	 * @return list[0] first element of the list
+	 */
+	{
+		if(count == 0)// if there are no elements
+		{
+			return -1;
+		}
+		return list[0];
+	}
+	public int last()
+	/**
+	 * Returns the last element of the list
+	 * @return list[count-1] last element of the list
+	 */
+	{
+		if(count == 0)// if there are no elements
+		{
+			return -1;
+		}
+		return list[count-1];
+	}
+	public int size()
+	/**
+	 * Returns the current number of possible locations in the list
+	 * @return list.length size of the list
+	 */
+	{
+		return list.length;
 	}
 }
